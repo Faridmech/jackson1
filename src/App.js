b96c2f1db595114c78b65383ca860c56e3521b61
Faridmech <feridh189@gmail.com>
2022-10-27 17:26:21 +0400

    jakson1 #3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style= {{
+      marign: "auto",
+      width: 800,
+      paddingTop:"1rem",
+    }}
+    >
+      <h1 className='title'>Ferid Huseynli</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Ad1</th>
+            <th>Ad2</th>
+            <th>Ad3</th>
+            <th>Ad4</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Ferid</td>
+            <td>Fuad</td>
+            <td>Ceyhun</td>
+            <td>Samir</td>
+          </tr>
+        </tbody>
+        
+      </table>
     </div>
   );
 }
